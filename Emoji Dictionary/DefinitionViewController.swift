7c10333e1c00487ce59066d4d98eb9bbc2b18370
Brian Hassett <bhassett.73@gmail.com>
2017-09-21 21:52:38 -0400

    passed emojis to 2nd view controller and added definitions

--- Emoji Dictionary/DefinitionViewController.swift
+++ Emoji Dictionary/DefinitionViewController.swift
@@ -9,11 +9,46 @@
 import UIKit
 
 class DefinitionViewController: UIViewController {
+    
+    @IBOutlet weak var definitionLabel: UILabel!
+    @IBOutlet weak var emojiLabel: UILabel!
+    var emoji = "NO EMOJI"
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        emojiLabel.text = emoji
+        
+        definitionLabel.text = "Definition goes here."
+        
+        // var emojis = ["😁","😎","😍","💩","😡","😱"]
+        
+        if emoji == "😁" {
+            definitionLabel.text = "Smiley Dude!"
+        }
+        
+        if emoji == "😎" {
+            definitionLabel.text = "Cool Dude!"
+        }
+        
+        if emoji == "😍" {
+            definitionLabel.text = "I love my wife!"
+        }
+        
+        if emoji == "💩" {
+            definitionLabel.text = "I hate poop!"
+        }
+        
+        if emoji == "😡" {
+            definitionLabel.text = "Poop makes me mad!"
+        }
+        
+        if emoji == "😱" {
+            definitionLabel.text = "Lots of poop is scary!"
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
