5753d1705553b4fc7f670ba3ecefeaa5b6dab101
Brian Hassett <bhassett.73@gmail.com>
2017-09-21 21:04:18 -0400

    added swift code file for 2nd veiwcontroller

---
+++ Emoji Dictionary/DefinitionViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DefinitionViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Brian Hassett on 9/21/17.
+//  Copyright © 2017 bhassett. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -34,7 +34,12 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "moveSegue", sender: "🗿")
+        let emoji = emojis[indexPath.row]
+        performSegue(withIdentifier: "moveSegue", sender: emoji)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
     }
 
     override func didReceiveMemoryWarning() {
